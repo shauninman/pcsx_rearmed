551ed2775bcc102b4d6090697ec1538e19c9304a
Shaun Inman <shaun@panic.com>
2021-04-04 23:27:05 -0400

    added tmp message on L2/R2 binding

--- frontend/menu.c
+++ frontend/menu.c
@@ -1089,11 +1089,107 @@ static void keys_load_all(const char *cfg)
 	in_clean_binds();
 }
 
+static void tmp_key_config_loop(const me_bind_action *opts, int opt_cnt, int player_idx)
+{
+	int i, sel = 0, menu_sel_max = opt_cnt - 1, does_combos = 0;
+	int dev_id, bind_dev_id, dev_count, kc, is_down, mkey;
+	int unbind, bindtype, mask_shift;
+
+	for (i = 0, dev_id = -1, dev_count = 0; i < IN_MAX_DEVS; i++) {
+		if (in_get_dev_name(i, 1, 0) != NULL) {
+			dev_count++;
+			if (dev_id < 0)
+				dev_id = i;
+		}
+	}
+
+	if (dev_id == -1) {
+		lprintf("no devs, can't do config\n");
+		return;
+	}
+
+	dev_id = -1; // show all
+	mask_shift = 0;
+	if (player_idx == 1)
+		mask_shift = 16;
+	bindtype = player_idx >= 0 ? IN_BINDTYPE_PLAYER12 : IN_BINDTYPE_EMU;
+	
+	for (;;)
+	{
+		draw_key_config(opts, opt_cnt, player_idx, sel, dev_id, dev_count, 0);
+		
+		menu_draw_begin(0, 1);
+		smalltext_out16(g_menuscreen_h - 20, (11 + 0) * me_mfont_h, "Hold MENU and", 0xffff);
+		smalltext_out16(g_menuscreen_h - 20, (11 + 1) * me_mfont_h, "press L1/R1", 0xffff);
+		smalltext_out16(g_menuscreen_h - 20, (11 + 2) * me_mfont_h, "to press L2/R2", 0xffff);
+		smalltext_out16(g_menuscreen_h - 20, (11 + 3) * me_mfont_h, "(or vice versa)", 0xffff);
+		menu_draw_end();
+		
+		mkey = in_menu_wait(PBTN_UP|PBTN_DOWN|PBTN_LEFT|PBTN_RIGHT
+				|PBTN_MBACK|PBTN_MOK|PBTN_MA2, NULL, 100);
+		switch (mkey) {
+			case PBTN_UP:   sel--; if (sel < 0) sel = menu_sel_max; continue;
+			case PBTN_DOWN: sel++; if (sel > menu_sel_max) sel = 0; continue;
+			case PBTN_LEFT:
+				for (i = 0, dev_id--; i < IN_MAX_DEVS + 1; i++, dev_id--) {
+					if (dev_id < -1)
+						dev_id = IN_MAX_DEVS - 1;
+					if (dev_id == -1 || in_get_dev_name(dev_id, 0, 0) != NULL)
+						break;
+				}
+				continue;
+			case PBTN_RIGHT:
+				for (i = 0, dev_id++; i < IN_MAX_DEVS; i++, dev_id++) {
+					if (dev_id >= IN_MAX_DEVS)
+						dev_id = -1;
+					if (dev_id == -1 || in_get_dev_name(dev_id, 0, 0) != NULL)
+						break;
+				}
+				continue;
+			case PBTN_MBACK:
+				return;
+			case PBTN_MOK:
+				if (sel >= opt_cnt)
+					return;
+				while (in_menu_wait_any(NULL, 30) & PBTN_MOK)
+					;
+				break;
+			case PBTN_MA2:
+				in_unbind_all(dev_id, opts[sel].mask << mask_shift, bindtype);
+				continue;
+			default:continue;
+		}
+
+		draw_key_config(opts, opt_cnt, player_idx, sel, dev_id, dev_count, 1);
+
+		/* wait for some up event */
+		for (is_down = 1; is_down; )
+			kc = in_update_keycode(&bind_dev_id, &is_down, NULL, -1);
+
+		i = count_bound_keys(bind_dev_id, opts[sel].mask << mask_shift, bindtype);
+		unbind = (i > 0);
+
+		/* allow combos if device supports them */
+		in_get_config(bind_dev_id, IN_CFG_DOES_COMBOS, &does_combos);
+		if (i == 1 && bindtype == IN_BINDTYPE_EMU && does_combos)
+			unbind = 0;
+
+		if (unbind)
+			in_unbind_all(bind_dev_id, opts[sel].mask << mask_shift, bindtype);
+
+		in_bind_key(bind_dev_id, kc, opts[sel].mask << mask_shift, bindtype, 0);
+
+		// make sure bind change is displayed
+		if (dev_id != -1)
+			dev_id = bind_dev_id;
+	}
+}
+
 static int key_config_loop_wrap(int id, int keys)
 {
 	switch (id) {
 		case MA_CTRL_PLAYER1:
-			key_config_loop(me_ctrl_actions, array_size(me_ctrl_actions) - 1, 0);
+			tmp_key_config_loop(me_ctrl_actions, array_size(me_ctrl_actions) - 1, 0);
 			break;
 		case MA_CTRL_PLAYER2:
 			key_config_loop(me_ctrl_actions, array_size(me_ctrl_actions) - 1, 1);
